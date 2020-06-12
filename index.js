42cc67c67c71a24e820a6554af7208d0864fc636
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-06-12 16:03:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+class Cat{
+  constructor(name,sex){
+    this.name =name
+    this.sex = sex
+  }
+speak(name){
+    return `${name} says meow`
+  }
+}
+
+class Dog{
+    constructor(name,sex){
+    this.name =name
+    this.sex = sex
+  }
+    speak(name){
+    return `${name} says woof`
+  }
+}
+
+class Bird{
+    constructor(name,sex){
+    this.name =name
+    this.sex = sex
+  }
+  
+   speak(name,sex){
+      if(sex === "male"){
+    return `It's me! ${name} , the parrot!`
+      }
+      else{
+        return `${name} says squawk!` 
+      }
+  }
+}

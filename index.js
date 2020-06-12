d6aa47de3fc91227c39bb25196a4f46e422dad70
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-06-12 16:07:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Cat{
     this.sex = sex
   }
 speak(){
-    return `${this.name} says meow`
+    return `${this.name} says meow!`
   }
 }
 
@@ -15,7 +15,7 @@ class Dog{
     this.sex = sex
   }
     speak(){
-    return `${this.name} says woof`
+    return `${this.name} says woof!`
   }
 }
 
